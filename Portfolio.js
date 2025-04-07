04b1c53044f7440e07d270230fc740a3c41e82ca
malou <ishybun@gmail.com>
2025-04-07 12:39:08 +0200

    worked a lot on the blog tab. i added an image gallery and wrote about the we are playgrounds event. i also added a ko-fi widget and the tab has a blog-like layout. i also added a home page which is active by default.

--- Portfolio.js
+++ Portfolio.js
@@ -1,21 +1,31 @@
-function openTab(evt, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+    //homepage open by default
+    window.onload = function() {
+    openTab(event = null, 'home');
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    function openTab(evt, tabName) {
+        var i, tabcontent, tablinks;
+
+        // Hide all tab contents
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        // Remove 'active' class from all tab links
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].classList.remove("active");
+        }
+
+        // Show the selected tab content
+        document.getElementById(tabName).style.display = "block";
+
+        // Add 'active' class to the clicked tab
+        if (evt) {
+            evt.currentTarget.classList.add("active");
+        } else {
+            // If there's no event (like on load), manually activate the first tab
+            document.querySelector(`.tablinks[onclick*="${tabName}"]`).classList.add("active");
+        }
     }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-
