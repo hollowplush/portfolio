8ceecffd82d269dc4c77267777bf8d673ccdec0b
malou <ishybun@gmail.com>
2025-04-08 14:48:41 +0200

    changed font and font size. began working on the learning outcomes page (added collapsibles). inside one of the collapsibles, i placed an image to try some animation stuff on.

--- Portfolio.js
+++ Portfolio.js
@@ -29,3 +29,19 @@
             document.querySelector(`.tablinks[onclick*="${tabName}"]`).classList.add("active");
         }
     }
+
+    //collapsible in learning outcomes tab
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
